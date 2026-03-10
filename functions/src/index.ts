ed909e5256f59ac461d51ff4991abc986121ca51
ramiromantero <ramiromantero@hotmail.com>
2026-03-09 23:57:02 -0300

    Fix2: arreglo package.json

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,5 @@
+import * as dotenv from "dotenv";
+dotenv.config(); // Carga las variables del archivo .env
 import * as functions from "firebase-functions";
 import { createApp } from "./server";
 
