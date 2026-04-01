7e9a19cf22ad28bf8ce7b59b044e8b2f5cc73d24
Agustina Landi <aglandi@fi.uba.ar>
2026-04-01 01:15:27 -0300

    feat: funciona ok el cambio de contraseña c link enviado x mail

--- functions/src/routes/auth.router.ts
+++ functions/src/routes/auth.router.ts
@@ -7,5 +7,6 @@ router.post("/login", AuthController.login);
 router.post("/register", AuthController.register);
 router.put("/profile", AuthController.updateProfile);
 router.post("/reset-password-request", AuthController.requestPasswordReset);
+router.post("/update-password", AuthController.updatePassword);
 
 export default router;
